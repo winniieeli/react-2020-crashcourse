189850187b40b268ec796fb308120f65ee75cc1f
winniieeli <37006584+winniieeli@users.noreply.github.com>
2020-05-25 21:40:02 -0700

    asdf

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,9 +6,18 @@ function Navigation() {
     const [showMenu, setShowMenu] = useState(false)
 
     let menu
+    let menuMask
+
     if (showMenu) {
-        menu = <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"> 
+        menu = <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadowg"> 
             This is the menu </div>
+
+        menuMask = 
+        <div 
+            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+            onClick={() => setShowMenu(false)}
+        >
+        </div>
     }
 
     return (
@@ -20,6 +29,7 @@ function Navigation() {
                 />
             </span>
             
+            { menuMask }
             { menu }
         </nav>
     );
