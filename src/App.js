bfb7322016009947a241a0d4691176f583cae339
winniieeli <37006584+winniieeli@users.noreply.github.com>
2020-05-26 13:39:15 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import HelloWorld from './components/HelloWorld'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
@@ -9,7 +15,18 @@ function App() {
       <Header />
 
       <HelloWorld name="Winnie"/>
-      
+
+      <Router>
+        <ul>
+          <li>
+            <Link to="/" className="text-blue-500">Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-blue-500">About</Link>
+          </li>
+        </ul>
+      </Router>
+
       <Footer />
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,22 @@
 import React, {useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBars} from '@fortawesome/free-solid-svg-icons'
+import {useTransition, animated} from 'react-spring'
 
 function Navigation() {
     const [showMenu, setShowMenu] = useState(false)
 
-    let menu
-    let menuMask
+    const maskTransitions = useTransition(showMenu, null, {
+        from: { position: 'absolute', opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
 
-    if (showMenu) {
-        menu = <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadowg"> 
-            This is the menu </div>
-
-        menuMask = 
-        <div 
-            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-            onClick={() => setShowMenu(false)}
-        >
-        </div>
-    }
+    const menuTransitions = useTransition(showMenu, null, {
+        from: { opacity: 0, transform: 'translateX(-100%)' },
+        enter: { opacity: 1, transform: 'translateX(0%)' },
+        leave: { opacity: 0, transform: 'translateX(-100%)' },
+    })
 
     return (
         <nav>
@@ -28,9 +26,39 @@ function Navigation() {
                     onClick={() => setShowMenu(!showMenu)}
                 />
             </span>
+
+            {
+                maskTransitions.map(({ item, key, props }) =>
+                item && 
+                <animated.div 
+                    key={key} 
+                    style={props}
+                    className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"                
+                    onClick={() => setShowMenu(false)}
+                >
+                </animated.div>
+                )
+            }
+
+            {
+                menuTransitions.map(({ item, key, props }) =>
+                item && 
+                <animated.div 
+                    key={key} 
+                    style={props}
+                    className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
+                >
+                    <span className="font-bold">
+                        The menu
+                    </span>
+                    <ul>
+                        <li>Home</li>
+                    </ul>
+                    
+                </animated.div>
+                )
+            }
             
-            { menuMask }
-            { menu }
         </nav>
     );
 }
